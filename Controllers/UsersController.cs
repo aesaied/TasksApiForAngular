ddcbe4695d2bc54aa26132200d54bd5ba203039c
Atallah Esaied <atallah.esaied@gmail.com>
2024-10-21 16:44:17 +0300

    Add project files.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TasksApi.Models;
+
+namespace TasksApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        [HttpPost("Register")]
+
+        public ActionResult Register(RegistrationDto input)
+        {
+
+            return Ok(input);
+
+        }
+    }
+}
---
+++ Models/RegistrationDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TasksApi.Models
+{
+    public class RegistrationDto
+    {
+
+        [Required]
+        [MaxLength(100)]
+        public string? FullName {  get; set; }
+
+        [Required]
+        [MaxLength (100)]
+        public string? UserName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)] // MVC
+        public string? Password { get; set; }
+
+        [Required]
+        [Compare(nameof(Password),ErrorMessage ="Confirm password doesn't match password")]
+        public string? ConfirmPassword { get; set; }
+    }
+}
